b1ebb49ddd7c29529da0b5764eb17c690c2d449b
Steven Lequient <steven06007@gmail.com>
2018-08-31 16:59:01 +0200

    retour sur du simple avant de faire la liste des checkpoints

--- main.cpp
+++ main.cpp
@@ -7,15 +7,47 @@ using namespace std;
 
 // Write an action using cout. DON'T FORGET THE "<< endl"
 // To debug: cerr << "Debug messages..." << endl;
+/*
+class Checkpoint {
+    private:
+        int number;   //"nom" du checkpoint : 0 est le point de depart
+        int x;
+        int y;
+	public:
+        Checkpoint(int t_number, int t_x, int t_y) : number(t_number), x(t_x), y(t_y) {}
+		bool operator==(const Checkpoint& a, const Checkpoint& b) {
+			if (a.getNumber() == b.getNumber() && a.getX() == b.getX() && a.getY() == b.getY()) return true;
+			return false;
+		}
+		int getNumber() const { return number; }
+		int getX() const { return x; }
+		int getY() const { return y; }
+};
+
+//Ajoute un nouveau checkpoint a la liste s'il est different du depart et de l'actuel, et retourne alors true dans ce cas
+bool addNewCheckpoint(vector<Checkpoint> checkpointList, Checkpoint currentCheckpoint, int currentCheckpointNumber){
+	//TODO: PASSAGES PAR POINTEUR ET ALLOCATION!
+	
+    //Pas de probleme sur back, qui n'est pas evalue si la liste est vide
+	if (checkpointList.empty() || checkpointList.back() != currentCheckpoint){
+		checkpointList.push_back(currentCheckpoint);
+		return true;
+	}
+	return false;
+}
+*/
+
 
 int main()
 {
     final int CHECKPOINT_APPROACH_DIST = 1000;
-    
+    /*
     vector<Checkpoint> checkpointList;
     int currentCheckpointNumber = 0;
-    
-
+	int lapNumber = 0;
+	int turnsSinceLastCheckpoint = 0;	//TODO
+	*/
+	
     // game loop
     while (1) {
         int x;
@@ -25,15 +57,21 @@ int main()
         int nextCheckpointDist; // distance to the next checkpoint
         int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
         cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
-        
-        Checkpoint currentCheckpoint = new Checkpoint
-        addNewCheckpoint
-        
         int opponentX;
         int opponentY;
         cin >> opponentX >> opponentY; cin.ignore();
-
-
+		
+		/*
+		//Le premier tour, ajoute les checkpoint apres leur passage
+        Checkpoint currentCheckpoint (currentCheckpointNumber, nextCheckpointX, nextCheckpointY);
+		if (lapNumber == 0){
+			bool checkpointJustPassed = addNewCheckpoint(checkpointList, currentCheckpoint, currentCheckpointNumber);
+			if (checkpointJustPassed){
+				currentCheckpointNumber++;
+				turnsSinceLastCheckpoint = 0;
+			}
+		}
+        */
         
         int thrust = 0;
         if (nextCheckpointAngle > 90 || nextCheckpointAngle < -90 || nextCheckpointDist < 1000){
@@ -51,15 +89,3 @@ int main()
     }
 }
 
-//Ajoute un nouveau checkpoint a la liste s'il est different du depart et de l'actuel
-void addNewCheckpoint(vector<Checkpoint> checkpointList, Checkpoint current, int checkpointNumber){
-    if ()
-}
-
-class Checkpoint{
-    public:
-        int number;   //0 est le point de depart
-        int x;
-        int y;
-        Checkpoint ()
-}
