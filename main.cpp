f43378b51c9a001bb667b4cfe2ff216e3ff7fd06
Steven Lequient <steven06007@gmail.com>
2018-09-02 18:31:30 +0200

    Marche aussi pour Gold!

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ using namespace std;
 
 const int MAP_WIDTH = 16000;
 const int MAP_HEIGHT = 9000;
-const int MAP_CENTER_X = MAP_WIDTH/2;
-const int MAP_CENTER_Y = MAP_HEIGHT/2;
+const int MAP_CENTER_X = MAP_WIDTH / 2;
+const int MAP_CENTER_Y = MAP_HEIGHT / 2;
 const int CHECKPOINT_RADIUS = 600;
 const int POD_RADIUS = 400;	//Important seulement pour les collisions : seul le centre du pod passe les checkpoints
 
@@ -45,47 +45,47 @@ and friction is 0.85
 
 /*
 class Checkpoint {
-    private:
-        int number;   //"nom" du checkpoint : 0 est le point de depart
-        int x;
-        int y;
-	public:
-        Checkpoint(int t_number, int t_x, int t_y) : number(t_number), x(t_x), y(t_y) {}
-		bool operator==(const Checkpoint& a, const Checkpoint& b) {
-			if (a.getNumber() == b.getNumber() && a.getX() == b.getX() && a.getY() == b.getY()) return true;
-			return false;
-		}
-		int getNumber() const { return number; }
-		int getX() const { return x; }
-		int getY() const { return y; }
+private:
+int number;   //"nom" du checkpoint : 0 est le point de depart
+int x;
+int y;
+public:
+Checkpoint(int t_number, int t_x, int t_y) : number(t_number), x(t_x), y(t_y) {}
+bool operator==(const Checkpoint& a, const Checkpoint& b) {
+if (a.getNumber() == b.getNumber() && a.getX() == b.getX() && a.getY() == b.getY()) return true;
+return false;
+}
+int getNumber() const { return number; }
+int getX() const { return x; }
+int getY() const { return y; }
 };
 
 //Ajoute un nouveau checkpoint a la liste s'il est different du depart et de l'actuel, et retourne alors true dans ce cas
 bool addNewCheckpoint(vector<Checkpoint> checkpointList, Checkpoint currentCheckpoint, int currentCheckpointNumber){
-	//TODO: PASSAGES PAR POINTEUR ET ALLOCATION!
-	
-    //Pas de probleme sur back, qui n'est pas evalue si la liste est vide
-	if (checkpointList.empty() || checkpointList.back() != currentCheckpoint){
-		
-		//TODO: ajouter le depart initialement si la liste est vide
-		
-		checkpointList.push_back(currentCheckpoint);
-		return true;
-	}
-	return false;
+//TODO: PASSAGES PAR POINTEUR ET ALLOCATION!
+
+//Pas de probleme sur back, qui n'est pas evalue si la liste est vide
+if (checkpointList.empty() || checkpointList.back() != currentCheckpoint){
+
+//TODO: ajouter le depart initialement si la liste est vide
+
+checkpointList.push_back(currentCheckpoint);
+return true;
+}
+return false;
 }
 */
 
 class Point {
-    private:
-        int x;
-        int y;
-	public:
-		Point(int t_x, int t_y);
-		bool operator== (const Point& p);
-		double getDistance(const Point& p);
-		int getX() const { return x; }
-		int getY() const { return y; }
+private:
+	int x;
+	int y;
+public:
+	Point(int t_x, int t_y);
+	bool operator== (const Point& p);
+	double getDistance(const Point& p);
+	int getX() const { return x; }
+	int getY() const { return y; }
 };
 
 Point::Point(int t_x, int t_y) {
@@ -98,68 +98,68 @@ bool Point::operator== (const Point& p) {
 	return false;
 }
 
-double Point::getDistance(const Point& p){
+double Point::getDistance(const Point& p) {
 	int distancex = (p.getX() - x) * (p.getX() - x);
 	int distancey = (p.getY() - y) * (p.getY() - y);
-	return sqrt(distancex + distancey); 
+	return sqrt(distancex + distancey);
 }
 
 
 
 int main()
 {
-    
-    /*
-    vector<Checkpoint> checkpointList;
+
+	/*
+	vector<Checkpoint> checkpointList;
 	int lapNumber = 0;
-	
+
 	*/
-	
+
 	//Permet temporairement de verifier si on a change de checkpoint
 	int previousCheckpointX = -1;
 	int previousCheckpointY = -1;
 	int turnsSinceLastCheckpoint = 0;
 	int checkpointNumber = 0;
 	bool isBoostAvailable = true;
-	
+
 	int previousX = 0;
 	int previousY = 0;
-	
-	
-    // game loop
-    while (1) {
-        int x;
-        int y;
-        int nextCheckpointX; // x position of the next check point
-        int nextCheckpointY; // y position of the next check point
-        int nextCheckpointDist; // distance to the next checkpoint
-        int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
-        cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
-        int opponentX;
-        int opponentY;
-        cin >> opponentX >> opponentY; cin.ignore();
-		
-        
-        int thrust = 0;
+
+
+	// game loop
+	while (1) {
+		int x;
+		int y;
+		int nextCheckpointX; // x position of the next check point
+		int nextCheckpointY; // y position of the next check point
+		int nextCheckpointDist; // distance to the next checkpoint
+		int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
+		cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+		int opponentX;
+		int opponentY;
+		cin >> opponentX >> opponentY; cin.ignore();
+
+
+		int thrust = 0;
 		int destinationX = nextCheckpointX;
 		int destinationY = nextCheckpointY;
-		
-		int velocityX = x-previousX;
-		int velocityY = y-previousY;
-		int velocity = (int)sqrt(velocityX*velocityX + velocityY*velocityY);
-		
-		Point pod (x, y);
-		Point nextCheckpoint (nextCheckpointX, nextCheckpointY);
-		
-		
+
+		int velocityX = x - previousX;
+		int velocityY = y - previousY;
+		int velocity = (int)sqrt(velocityX*velocityX + velocityY * velocityY);
+
+		Point pod(x, y);
+		Point nextCheckpoint(nextCheckpointX, nextCheckpointY);
+
+
 		//Si le checkpoint est derriere nous, on accellere pas, pour ne pas s'en eloigner
-        if (nextCheckpointAngle > 90 || nextCheckpointAngle < -90){
-            thrust = 0;
-        }
-		else{
+		if (nextCheckpointAngle > 90 || nextCheckpointAngle < -90) {
+			thrust = 0;
+		}
+		else {
 			thrust = 100;
-			
-			
+
+
 			//On ralentit en approchant
 			if (nextCheckpointDist < CHECKPOINT_APPROACH_DIST_1) {
 				thrust = CHECKPOINT_APPROACH_THRUST_1;
@@ -170,12 +170,12 @@ int main()
 			if (nextCheckpointDist < CHECKPOINT_APPROACH_DIST_3) {
 				thrust = CHECKPOINT_APPROACH_THRUST_3;
 			}
-			
+
 
 
 			//Calcul de la position approximative du tour suivant, pour verifier si on sera dans le checkpoint
 			Point nextPosition(x + 3 * velocityX, y + 3 * velocityY);
-			
+
 			cerr << "nextCheckpointX : " << x + 2 * velocityX << endl;
 			cerr << "nextCheckpointY : " << y + 2 * velocityY << endl;
 			cerr << "nextCheckpoint.getDistance(nextPosition) : " << nextCheckpoint.getDistance(nextPosition) << endl;
@@ -186,28 +186,32 @@ int main()
 				destinationX = MAP_CENTER_X;
 				destinationY = MAP_CENTER_Y;
 			}
-			
-			
+
+
 			//Si le boost est disponible et qu'on est assez loin, on boost
 			if (isBoostAvailable
-					&& nextCheckpointDist >= CHECKPOINT_BOOST_APPROACH_DIST
-					&& nextCheckpointAngle < 20
-					&& nextCheckpointAngle > -20){
-			    thrust = 999;   //BOOST
-			    isBoostAvailable = false;
+				&& nextCheckpointDist >= CHECKPOINT_BOOST_APPROACH_DIST
+				&& nextCheckpointAngle < 20
+				&& nextCheckpointAngle > -20) {
+				thrust = 999;   //BOOST
+				isBoostAvailable = false;
 			}
-        }
-		
+		}
+
+
+		if (thrust <= 100 && thrust >= 0) {
+			cout << destinationX << " " << destinationY << " " << thrust << " " << thrust << endl;
+		}
+		if (thrust > 100) {
+			cout << destinationX << " " << destinationY << " " << "BOOST BOOST" << endl;
+		}
+		if (thrust < 0) {
+			cout << destinationX << " " << destinationY << " " << "SHIELD SHIELD" << endl;
+		}
+
 		turnsSinceLastCheckpoint++;
 		previousX = x;
 		previousY = y;
-		if (thrust <= 100){
-		    cout << destinationX << " " << destinationY << " " << thrust << " " << thrust << endl;
-		}
-		else{
-			cout << destinationX << " " << destinationY << " " << "BOOST BOOST" << endl;
-		}
-    }
+
+	}
 }
-
-
